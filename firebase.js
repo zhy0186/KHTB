c71233c07776227423d9dcbf7cd24def1042fcb2
zhy0186 <taozhengyan666@gmail.com>
2026-06-26 00:34:02 +0800

    Add Firebase initialization and Firestore setup

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyB3jEGxVxty9N8gAQyXh4ozyWbMqhXaOMA",
+  authDomain: "khtb-d09ad.firebaseapp.com",
+  projectId: "khtb-d09ad",
+  storageBucket: "khtb-d09ad.firebasestorage.app",
+  messagingSenderId: "543151767116",
+  appId: "1:543151767116:web:3c8e5ed1a2929ad3877ee9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore Database
+const db = getFirestore(app);
+
+// Export
+export { db };
