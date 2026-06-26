a715bde6dfbc5d0514e2f582ea8ebe8561d7d92b
zhy0186 <taozhengyan666@gmail.com>
2026-06-26 23:50:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,79 @@
 import { db } from "./firebase.js";
 
 import {
-  collection,
-  getDocs,
-  doc,
-  updateDoc
+collection,
+getDocs,
+doc,
+updateDoc
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const inventoryContainer =
-  document.getElementById("inventoryContainer");
+document.getElementById("inventoryContainer");
 
 // 读取库存
 async function loadInventory() {
 
-  inventoryContainer.innerHTML =
-    "<p>Loading...</p>";
+inventoryContainer.innerHTML =
+"<p>Loading...</p>";
 
-  const querySnapshot =
-    await getDocs(collection(db, "inventory"));
+const querySnapshot =
+await getDocs(collection(db, "inventory"));
 
-  inventoryContainer.innerHTML = "";
+inventoryContainer.innerHTML = "";
 
-  querySnapshot.forEach((docSnap) => {
+querySnapshot.forEach((docSnap) => {
 
-    const item = docSnap.data();
+```
+const item = docSnap.data();
 
-    const card = document.createElement("div");
+const card = document.createElement("div");
 
-    card.className = "inventory-card";
+card.className = "inventory-card";
 
-    card.innerHTML = `
-      <h3>${item.name_en}</h3>
-      <p>${item.name_cn}</p>
+card.innerHTML = `
+  <h3>${item.name_en}</h3>
+  <p>${item.name_cn}</p>
 
-      <div class="stock">
-        ${item.stock} pcs
-      </div>
+  <div class="stock">
+    ${item.stock} pcs
+  </div>
 
-      ${
-        item.stock < 10
-        ? '<div class="low-stock">⚠ Low Stock</div>'
-        : ''
-      }
+  ${
+    item.stock < 10
+      ? '<div class="low-stock">⚠ Low Stock</div>'
+      : ''
+  }
 
-      <button class="take-btn">
-       TEST BUTTON
-      </button>
-    `;
+  <button class="take-btn">
+    TEST BUTTON
+  </button>
+`;
 
-    const takeBtn =
-      card.querySelector(".take-btn");
+const takeBtn =
+  card.querySelector(".take-btn");
 
-    takeBtn.addEventListener("click", async () => {
+takeBtn.addEventListener("click", async () => {
 
-      if (item.stock <= 0) {
-        alert("Out of Stock");
-        return;
-      }
+  if (item.stock <= 0) {
+    alert("Out of Stock");
+    return;
+  }
 
-      await updateDoc(
-        doc(db, "inventory", docSnap.id),
-        {
-          stock: item.stock - 1
-        }
-      );
+  await updateDoc(
+    doc(db, "inventory", docSnap.id),
+    {
+      stock: item.stock - 1
+    }
+  );
 
-      loadInventory();
+  loadInventory();
 
-    });
+});
 
-    inventoryContainer.appendChild(card);
+inventoryContainer.appendChild(card);
+```
 
-  });
+});
 
 }
 
